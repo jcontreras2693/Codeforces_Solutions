587b31b010b7ab3978004b30863cb4ae3e547116
Juan Contreras <judacobe2693@gmail.com>
2025-03-12 18:06:10 -0500

    ECI_Contest

---
+++ ECI_Contest/Books.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+def sliding_window(n, t, times):
+    
+    
+    return 
+
+def main():
+    n, t = map(int, stdin.readline().strip().split())
+    times = list(map(int, stdin.readline().strip().split()))
+
+    print(sliding_window(n, t, times))
+
+
+if __name__=="__main__":
+    main()
---
+++ ECI_Contest/IsItRated.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+def main():
+    for i in stdin:
+        print("NO", flush=True)
+
+if __name__ == "__main__":
+    main()
